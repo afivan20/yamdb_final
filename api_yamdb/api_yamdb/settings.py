b135c781eb2003f8ca24264aaee143714679c50b
Ivan <afivan@bk.ru>
2022-03-19 16:51:24 +0300

    email

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.aol.com'
 EMAIL_HOST_USER = 'pupkin_v'
 EMAIL_HOST_PASSWORD = 'plzletmein'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 #####
