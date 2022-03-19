18eb52209596aa595afa1f4a5fd5b24e6cf7fc63
Ivan <afivan@bk.ru>
2022-03-19 21:03:32 +0300

    secret_django

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 
 from dotenv import load_dotenv
 
+env_path = Path('/infra/')/'.env'
+load_dotenv(dotenv_path=env_path)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
