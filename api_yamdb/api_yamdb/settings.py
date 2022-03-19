3c11f43a825e36a4d969a04cbc2aeddd12f7b10d
Ivan <afivan@bk.ru>
2022-03-19 21:59:28 +0300

    completed

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,12 +2,12 @@ import os
 from datetime import timedelta
 
 from dotenv import load_dotenv
-
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET', default='секретная-тайна')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
